41885bc45ea7493c7a52433baab70bc5185b71a4
TJ <tjchase34@gmail.com>
2019-05-14 22:24:24 -0400

    Woah woah woah

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public float jumpVelocity;
     public Vector2 velocity;
+    public float gravity;
     public LayerMask wallMask;
+    public LayerMask floorMask;
+
     private bool walk, walk_left, walk_right, jump;
+
+    public enum PlayerState {
+        jumping,
+        idle,
+        walking
+    }
+
+    private PlayerState playerState = PlayerState.idle;
+
+    private bool grounded = false;
 
     // Start is called before the first frame update
     void Start() {
-        
+        Fall();
     }
 
     // Update is called once per frame
@@ -33,10 +47,21 @@ public class Player : MonoBehaviour {
                 pos.x += velocity.x * Time.deltaTime;
                 scale.x = 1;
             }
-
             pos = CheckWallRays(pos, scale.x);
-
         }
+
+        if (jump && playerState != PlayerState.jumping) {
+            playerState = PlayerState.jumping;
+            velocity = new Vector2(velocity.x, jumpVelocity);
+        }
+
+        if (playerState == PlayerState.jumping) {
+            pos.y += velocity.y * Time.deltaTime;
+            velocity.y -= gravity * Time.deltaTime;
+        }
+
+        if (velocity.y <= 0)
+            pos = CheckFloorRays(pos);
 
         transform.localPosition = pos;
         transform.localScale = scale;
@@ -68,11 +93,59 @@ public class Player : MonoBehaviour {
         RaycastHit2D wallMiddle = Physics2D.Raycast(originMiddle, new Vector2(direction, 0), velocity.x * Time.deltaTime, wallMask);
         RaycastHit2D wallBottom = Physics2D.Raycast(originBottom, new Vector2(direction, 0), velocity.x * Time.deltaTime, wallMask);
 
-        if (wallTop.collider != null || wallMiddle.collider != null || wallBottom.collider != null) {
+        // if (wallTop.collider != null || wallMiddle.collider != null || wallBottom.collider != null) {
+        if (wallTop.collider != null || wallMiddle.collider != null) {
             pos.x -= velocity.x * Time.deltaTime * direction;
         }
 
         return pos;
 
     }
+
+    Vector3 CheckFloorRays(Vector3 pos) {
+
+        Vector2 originLeft = new Vector2(pos.x - 0.5f + 0.2f, pos.y - 1f);
+        Vector2 originMiddle = new Vector2(pos.x, pos.y - 1f);
+        Vector2 originRight = new Vector2(pos.x + 0.5f - 0.2f, pos.y - 1f);
+
+        RaycastHit2D floorLeft = Physics2D.Raycast(originLeft, Vector2.down, velocity.y * Time.deltaTime, floorMask);
+        RaycastHit2D floorMiddle = Physics2D.Raycast(originMiddle, Vector2.down, velocity.y * Time.deltaTime, floorMask);
+        RaycastHit2D floorRight = Physics2D.Raycast(originRight, Vector2.down, velocity.y * Time.deltaTime, floorMask);
+
+        if (floorLeft.collider != null || floorMiddle.collider != null || floorRight.collider != null) {
+
+            RaycastHit2D hitRay = floorRight;
+
+            if (floorLeft) {
+                hitRay = floorLeft;
+            } else if (floorMiddle) {
+                hitRay = floorMiddle;
+            } else if (floorRight) {
+                hitRay = floorRight;
+            } 
+
+            playerState = PlayerState.idle;
+            grounded = true;
+            velocity.y = 0;
+
+            pos.y = hitRay.collider.bounds.center.y + hitRay.collider.bounds.size.y / 2 + 0.5f;
+        } else {
+
+            if (playerState != PlayerState.jumping) {
+                Fall();
+            }
+        }
+
+        return pos;
+
+    }
+
+    void Fall() {
+        velocity.y = 0;
+        playerState = PlayerState.jumping;
+        grounded = false;
+    }
+
+
+
 }
